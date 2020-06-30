d9116d7a13f93d40bb2ddad683900981bd2a687b
avit sidis <avit.sidis@gmail.com>
2020-06-30 23:17:52 +0200

    Add project files.

---
+++ src/Demo.Hosting/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Demo.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Hosting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService weatherForecastService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
+        {
+            _logger = logger;
+            this.weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return weatherForecastService.Get();
+        }
+    }
+}
